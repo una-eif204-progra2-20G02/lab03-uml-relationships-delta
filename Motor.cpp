404209961c6b606846ebcb44e1d024c09fe68223
Deiner Rodriguez <deiner.ra10@gmail.com>
2020-08-11 18:16:51 +0000

    Creacion de la clase Motor

--- Motor.cpp
+++ Motor.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Motor.h"
+
+Motor::Motor(int _cilindraje, std::string _marca){
+  cilindraje=_cilindraje;
+  marca=_marca;
+}
+
+int Motor::get_cilindraje() { return cilindraje; }
+void Motor::set_cilindraje(int x) { cilindraje = x; }
+
+std::string Motor::get_marca() { return marca; }
+void Motor::set_marca(std::string x) { marca = x; }
--- Motor.h
+++ Motor.h
@@ -4,10 +4,21 @@
 
 #ifndef BASIC_00_EXAMPLE_MOTOR_H
 #define BASIC_00_EXAMPLE_MOTOR_H
-
+#include<string>
 
 class Motor {
 
+ int cilindraje;
+ std::string marca;
+
+ public:
+	 Motor(int = 0, std::string = " ");
+
+	 std::string get_marca();
+	 void set_marca(std::string);
+
+	 int get_cilindraje();
+	 void set_cilindraje(int);
 };
 
 
